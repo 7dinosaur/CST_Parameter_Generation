e85e5b021fe7de8bbe0758e9625d97b441ad9f55
7dinosaur <1604331440@qq.com>
2026-03-06 14:15:52 +0800

    panel

--- aircraft_gene.py
+++ aircraft_gene.py
@@ -6,6 +6,19 @@ import scipy.interpolate as si
 from scipy.special import comb
 from matplotlib import pyplot as plt
 
+def deri_1d(x, y):
+    assert len(x) == len(y), "xy应有相同形状"
+    deri = np.zeros([len(x),])
+    for i in range(len(x)):
+        if i == 0:
+            deri[i] = (-3*y[i] + 4*y[i+1] - y[i+2])/(x[i+2] - x[i])
+        elif i == len(x)-1:
+            deri[i] = (3*y[i] - 4*y[i-1] + y[i-2])/(x[i] - x[i-2])
+        else:
+            deri[i] = (y[i+1] - y[i-1])/(x[i+1] - x[i-1])
+
+    return deri
+
 class Aircraft:
     def __init__(self) -> None:
         self.origin_para:ndarray = np.array([0])
@@ -79,11 +92,12 @@ class Aircraft:
 
         self.air_mesh = mesh
     
-    def gene_panel_mesh(self, para) -> None:
+    def gene_panel_mesh(self) -> list[ndarray]:
         """生成三维网格数组,第一维为dom编号,如aircraft[0]=dom1,二三维为ij方向,四维[x,y,z]"""
         """panel_mesh是可以直接输入faboom程序计算的分块网格"""
 
         this_para = self.interp_para(61) ##此处插值仅保证曲线光滑，实际网格尺度与插值长度无关
+        order = self.cst_order
 
         ##统一网格尺度设置
         nose_i, body_i, tail_i = 31, 60, 10
@@ -92,9 +106,30 @@ class Aircraft:
         wing_j = 29
 
         ##头部网格计算
-        dom1, dom2 = np.zeros([nose_i, nose_j]), np.zeros([nose_i, nose_j])
+        dom1, dom2 = np.zeros([nose_i, nose_j]), np.zeros([nose_i, nose_j]) ##机头网格
+        dom3, dom4 = np.zeros([body_i, body_j]), np.zeros([body_i, body_j]) ##机身网格
+        dom5 = np.zeros([body_i, 2]) ##机翼上表面与尾涡面相连的网格
+        dom6, dom7 = np.zeros([wing_i, wing_j-1]), np.zeros([wing_i, wing_j-1]) ##机翼网格
+        dom8 = np.zeros([body_i, 2]) ##翼尖网格
+        dom9, dom10 = np.zeros([tail_i, tail_j]), np.zeros([tail_i, tail_j]) ##尾部网格
+        dom11 = np.zeros([tail_j, 2]) ##钝底网格
+        dom12 = np.zeros([tail_i+1, 2]) ##尾涡面网格
+
+        ##机头网格计算
+        #===================================#
+        leading_edge = this_para[:, 2*order+3]
+        leading_deri = deri_1d(leading_edge, this_para[:, 0])
+        # plt.plot(leading_edge, this_para[:, 0])
+        plt.plot(leading_edge, leading_deri)
+        #===================================#
+
+        ##机身网格计算
+        #===================================#
+        #===================================#
         
         panel_mesh = [locals()[f"dom{i}"] for i in range(1, 12)] ##由于分块网格长度尺度不统一用列表存储
+
+        return panel_mesh
     
     def write_mesh(self, mesh:NDArray | list, file_path:str) -> None:
         with open(file_path, 'w') as f:
@@ -111,6 +146,13 @@ class Aircraft:
                     for line in dom:
                         f.write(" ".join(f"{x:.6f}" for x in line) + "\n")
                 print(f"写入完毕,网格形状为[{n_i},{n_j}]. 网格文件路径：{file_path}")
+
+            elif type(mesh) == list: #panel mesh
+                print("写入面元网格...")
+                f.write(f"{len(mesh)}\n")
+                for dom in mesh:
+                    n_i, n_j = dom.shape[0], dom.shape[1]
+                    f.write(f"{n_i} {n_j} 1\n")
     
 class Aircraft_generator:
     def __init__(self, ) -> None:
@@ -119,9 +161,8 @@ class Aircraft_generator:
 if __name__ == "__main__":
     air_para = Aircraft()
     air_para.read_from_csv("increase_cabin.csv")
-    air_para.gene_simple_mesh(41, 60)
-    test_mesh = air_para.air_mesh
+    # air_para.gene_simple_mesh(41, 60)
+    test_mesh = air_para.gene_panel_mesh()
+    # test_mesh = air_para.air_mesh
     air_para.write_mesh(test_mesh, "test_mesh.x")
-    plt.plot(test_mesh[0, 0, :, 0], test_mesh[0, 0, :, 2])
-    plt.plot(test_mesh[1, 0, :, 0], test_mesh[1, 0, :, 2])
     plt.show()
