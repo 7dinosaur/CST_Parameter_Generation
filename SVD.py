040aea5d6201c5d1d9f1620387e7441631df57ab
7dinosaur <1604331440@qq.com>
2026-03-20 10:24:40 +0800

    daka

--- SVD.py
+++ SVD.py
@@ -2,6 +2,7 @@ import pandas as pd
 import numpy as np
 from numpy import ndarray
 from aircraft_gene import Aircraft
+from matplotlib import pyplot as plt
 
 def SVD(X:ndarray, k):
     # 展平
@@ -11,12 +12,14 @@ def SVD(X:ndarray, k):
     X_centered = X - mean
     # 分解
     U, s, Vt = np.linalg.svd(X_centered, full_matrices=False)
+    plt.plot(*list(range(1, len(s)+1)), s)
+    plt.ylim(0, 1)
     U_k, s_k, Vt_k = U[:, :k], s[:k], Vt[:k, :]
     X_reduced = U_k @ np.diag(s_k)  # 降维结果
-    print(X_reduced.shape)
-    coef = 3
+    print(s)
+    coef = 1
     var_min, var_max = -coef*s_k, +coef*s_k
-    print(f"变量下限：{var_min}, 变量上限：{var_max}")
+    # print(f"变量下限：{var_min}, 变量上限：{var_max}")
     # 重构
     X_recon_centered = X_reduced @ Vt_k  # 回到 192 维，但中心化
     X_recon = X_recon_centered + mean    # 加回均值
@@ -32,9 +35,9 @@ def SVD_recon(var, Vt_k, mean):
     return X_recon
 
 if __name__ == "__main__":
-    data_path = "filter_solutions_v3.csv"
+    data_path = "filter_solutions_v1_set.csv"
     data = pd.read_csv(data_path).to_numpy()[:, 3:] # 每行代表一个样本的飞机参数, 如(14,192)代表14个样本
-    k = 2
+    k = 5
     X_reduced, Vt_k, mean, var_min, var_max = SVD(data, k)
     var_test = X_reduced[0]
     recon_sample = SVD_recon(var_test, Vt_k, mean)
@@ -43,3 +46,4 @@ if __name__ == "__main__":
     para = recon_sample.reshape(-1, 24)
     test_air = Aircraft(para)
     test_air.write_mesh("panel", "check.x", 0)
+    plt.show()
