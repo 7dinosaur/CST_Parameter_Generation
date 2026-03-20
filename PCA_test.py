57fda1f48d0852481ffe101ed12c725a5dee90ec
7dinosaur <1604331440@qq.com>
2026-03-20 17:31:48 +0800

    PCA

--- PCA_test.py
+++ PCA_test.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import numpy as np
 from matplotlib import pyplot as plt
+from aircraft_gene import Aircraft
 
 class aircraft_para:
     def __init__(self, origin_X) -> None:
@@ -11,7 +12,6 @@ class aircraft_para:
     def PCA(self, k):
         mean = np.mean(self.X, axis=0)
         X_center = self.X - mean
-        print(X_center.shape)
         Cov = X_center.T @ X_center / (self.n_sample - 1)
         U, s = np.linalg.eigh(Cov)
         U = U[::-1] #奇异值
@@ -19,7 +19,15 @@ class aircraft_para:
         ## 取前k个主成分
         s_k = s[:, :k]
         Z = X_center @ s_k
-        X_recon = Z @ s_k.T + mean
+        Z_min = np.min(Z, axis=0)  # (k,)
+        Z_max = np.max(Z, axis=0)  # (k,)
+        test_Z = np.random.uniform(Z_min, Z_max)
+        X_recon = test_Z @ s_k.T + mean
+        X_recon = X_recon.reshape(-1, 24)
+        print(X_recon.shape)
+        air_test = Aircraft(X_recon)
+        air_test.write_mesh("panel", 'check.x', 0)
+        print(X_recon.shape)
         plt.plot(list(range(len(U))), U)
 
     def kernel_PCA(self):
@@ -29,6 +37,6 @@ if __name__ == "__main__":
     data_path = "qualified_solutions_1_set.csv"
     data = pd.read_csv(data_path).to_numpy()[:, 3:]
     para = aircraft_para(data)
-    k = 6
-    para.PCA(5)
+    k = 5
+    para.PCA(k)
     plt.show()
